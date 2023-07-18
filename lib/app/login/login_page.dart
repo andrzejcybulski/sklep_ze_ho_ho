e03ee1ee44759c12b86bbcb894f548249a6f3916
Andrzej Cybulski <andrzej_cybulski@o2.pl>
2023-07-18 20:27:36 +0200

    Add login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,93 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     super.key,
   });
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Jesteś niezalogowany'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Sklep Że Ho Ho'),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const CircleAvatar(
+            backgroundImage: AssetImage('images/santa1.jpeg'),
+            radius: 80,
+          ),
+          const SizedBox(height: 20),
+          const Text('Logowanie'),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'E-mail',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Hasło',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+              ),
+              obscureText: true,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              shape: const StadiumBorder(),
+            ),
+            child: const Text('Zaloguj się'),
+            onPressed: () async {
+              try {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: emailController.text,
+                  password: passwordController.text,
+                );
+              } catch (error) {
+                print(error);
+              }
+            },
+          ),
+          TextButton(
+            child: const Text('Masz już konto? Zaloguj się'),
+            onPressed: () {},
+          ),
+          TextButton(
+            child: const Text('Nie masz konta? Zarejestruj się'),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Że Ho Ho sklep';
     return MaterialApp(
-      title: appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
@@ -49,87 +47,3 @@ class RootPage extends StatelessWidget {
         });
   }
 }
-
-
-
-
-          
-        
-        
-
-
-// class RootPage extends StatelessWidget {
-//   const RootPage({
-//     super.key,
-//     required this.appTitle,
-//   });
-
-//   final String appTitle;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<User?>(
-//         stream: FirebaseAuth.instance.authStateChanges(),
-//         builder: (context, snapshot) {
-//           final user = snapshot.data;
-//           if (user == null) {}
-//           return Scaffold(
-//             appBar: AppBar(
-//               title: Text(appTitle),
-//               backgroundColor: Colors.red,
-//             ),
-//             body: Column(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: [
-//                 // const SizedBox(height: 20),
-//                 // // const Image(
-//                 // //   image: AssetImage('images/santa1.jpeg'),
-//                 // //   width: 150,
-//                 // // ),
-//                 // const CircleAvatar(
-//                 //   backgroundImage: AssetImage('images/santa1.jpeg'),
-//                 //   radius: 80,
-//                 // ),
-//                 // const SizedBox(height: 20),
-//                 // const Text('Logowanie'),
-//                 // const SizedBox(height: 30),
-//                 // Padding(
-//                 //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-//                 //   child: TextFormField(
-//                 //     decoration: const InputDecoration(
-//                 //       border: OutlineInputBorder(),
-//                 //       hintText: 'Enter your e-mail',
-//                 //     ),
-//                 //   ),
-//                 // ),
-//                 // const SizedBox(height: 20),
-//                 // Padding(
-//                 //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-//                 //   child: TextFormField(
-//                 //     decoration: const InputDecoration(
-//                 //       border: OutlineInputBorder(),
-//                 //       hintText: 'Enter your password',
-//                 //     ),
-//                 //   ),
-//                 // ),
-//                 // ElevatedButton(
-//                 //   style: ElevatedButton.styleFrom(
-//                 //     backgroundColor: Colors.red,
-//                 //   ),
-//                 //   child: const Text('Zaloguj się'),
-//                 //   onPressed: () {},
-//                 // ),
-//                 // // TextButton(
-//                 // //   child: Text('Masz już konto? Zaloguj się'),
-//                 // //   onPressed: () {},
-//                 // // ),
-//                 // // TextButton(
-//                 // //   child: Text('Nie masz konta? Zarejestruj się'),
-//                 // //   onPressed: () {},
-//                 // // ),
-//               ],
-//             ),
-//           );
-//         });
-//   }
-// }
